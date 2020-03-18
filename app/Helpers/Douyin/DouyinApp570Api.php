42763f6e352d07c46b2913a60bfb95a7027fc313
peimeng <931692760@qq.com>
2020-03-19 01:26:37 +0800

    horizon

---
+++ app/Helpers/Douyin/DouyinApp570Api.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Helpers\Douyin;
+
+
+class DouyinApp570Api
+{
+    public function taskList($cookie)
+    {
+
+    }
+
+    public function taskInfo($task_id, $cookie)
+    {
+
+    }
+
+    protected function getXg($url)
+    {
+
+    }
+}
