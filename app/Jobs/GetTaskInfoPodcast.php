aa4533f36f0cb3e5693c97d0fba6f994e09411aa
peimeng <931692760@qq.com>
2020-03-23 21:17:09 +0800

    task info bug "Arr not found"

--- app/Jobs/GetTaskInfoPodcast.php
+++ app/Jobs/GetTaskInfoPodcast.php
@@ -14,6 +14,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Arr;
 
 class GetTaskInfoPodcast implements ShouldQueue
 {
