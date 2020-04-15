75b1c7f2024330a064b111358bc74388c9b4903a
peimeng <931692760@qq.com>
2020-04-15 21:28:50 +0800

    获取视频列表test

--- tests/Feature/DouyinApiTest.php
+++ tests/Feature/DouyinApiTest.php
@@ -3,12 +3,23 @@
 namespace Tests\Feature;
 
 use App\Helpers\Douyin\DouyinApp570Api;
+use App\Helpers\Douyin\DouyinWebApi;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class DouyinApiTest extends TestCase
 {
+    public function testGetAwemePost()
+    {
+        $cookie = 'sessionid=1a904662ffdfcd8fb565d800c491b00d';
+        $api = new DouyinWebApi();
+
+        $res = $api->getAwemePost($cookie);
+
+        $this->assertTrue($res['status_code'] === 0);
+    }
+
     public function testGetSubPid()
     {
         $cookie = 'sessionid=e8385bab1eb25b504855f3b7980945ce';
@@ -31,13 +42,13 @@ class DouyinApiTest extends TestCase
 
     public function testGetTaskInfo()
     {
-        $cookie = 'sessionid=1f1e33a469390c49a75f705d16f7fb85';
-        $task_id = '1661142418640983';
+        $cookie = 'sessionid=f3e1f0c1131490a3d0eb55295df34f16';
+        $task_id = '6815776395084451084';
 
         $api = new DouyinApp570Api();
 
         $res = $api->taskInfo($task_id, $cookie);
-
+        dd($res);
         $this->assertTrue($res['status_code'] === 0);
     }
 
@@ -48,7 +59,7 @@ class DouyinApiTest extends TestCase
         $api = new DouyinApp570Api();
 
         $res = $api->getShopPromotion($aweme_id);
-        dd($res);
+
         $this->assertTrue($res['status_code'] === 0);
     }
 }
