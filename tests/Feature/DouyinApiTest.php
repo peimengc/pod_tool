2d34770a25a547c6cd5ad3ce78b3e5754d13a93a
peimeng <931692760@qq.com>
2020-04-18 20:44:58 +0800

    null

--- tests/Feature/DouyinApiTest.php
+++ tests/Feature/DouyinApiTest.php
@@ -10,56 +10,19 @@ use Tests\TestCase;
 
 class DouyinApiTest extends TestCase
 {
-    public function testGetAwemePost()
+    public function test570Api()
     {
-        $cookie = 'sessionid=1a904662ffdfcd8fb565d800c491b00d';
-        $api = new DouyinWebApi();
-
-        $res = $api->getAwemePost($cookie);
-
-        $this->assertTrue($res['status_code'] === 0);
-    }
-
-    public function testGetSubPid()
-    {
-        $cookie = 'sessionid=e8385bab1eb25b504855f3b7980945ce';
-        $api = new DouyinApp570Api();
-
-        $res = $api->getSubPid($cookie);
-
-        $this->assertTrue($res['status_code'] === 0);
-    }
-
-    public function testGetTaskList()
-    {
-        $cookie = 'sessionid=1f1e33a469390c49a75f705d16f7fb85';
-        $api = new DouyinApp570Api();
-
-        $res = $api->taskList($cookie);
-
-        $this->assertTrue($res['status_code'] === 0);
-    }
-
-    public function testGetTaskInfo()
-    {
-        $cookie = 'sessionid=f3e1f0c1131490a3d0eb55295df34f16';
+        $cookie = 'sessionid=ed8f742341199ecc60142e2a06fc7b43';
         $task_id = '6815776395084451084';
+
+//        $cookie = 'sessionid=027f5d98730ad15d8115a7f51f31fd3f';
+//        $task_id = '1663945943893015';
 
         $api = new DouyinApp570Api();
 
         $res = $api->taskInfo($task_id, $cookie);
-        dd($res);
-        $this->assertTrue($res['status_code'] === 0);
-    }
-
-    public function testShopPromotion()
-    {
-        $aweme_id = '6812555208627932420';
-
-        $api = new DouyinApp570Api();
-
-        $res = $api->getShopPromotion($aweme_id);
 
         $this->assertTrue($res['status_code'] === 0);
     }
+
 }
